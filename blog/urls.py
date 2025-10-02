a24b5eb87dc7e80595f98f6a318358b81ac93be3
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-10-02 22:43:59 +0300

    14-й коммит, регистрация пространства имен в основном файле urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from blog.views import PostListView, PostDetailView
 from django.urls import path
 
 urlpatterns = [
-    path('', PostListView.as_view()),
-    path('<int:pk>/', PostDetailView.as_view()),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]

c13f11c5775fd2643bbffb1730cff4d895706b20
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-10-02 22:35:12 +0300

    13-й коммит, шаблоны, представления

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
 
 class PostListView(ListView):
+    # Для просмотра списка опубликованных статей
     model = Post
-    template_name = 'blog/index.html'
+    template_name = 'blog/post/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post/post_detail.html'
+    context_object_name = 'post'

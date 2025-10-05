23d6ba84f367937e5b650fe54f71bf40ec9483ea
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-10-05 21:34:32 +0300

    35-й коммит, модификация канонического URL-адреса постов

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]

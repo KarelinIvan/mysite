8e2f67ac594bfb854b0987b1793395dd4e2946dc
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-10-05 22:56:42 +0300

    37-й коммит, правка

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,1 @@
-from django import forms
 
-
-class EditPostForm(forms.Form):
-    """ Форма для отправки поста по e-mail """
-    name = forms.CharField(label='Имя', max_length=100)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comment = forms.CharField(widget=forms.Textarea)

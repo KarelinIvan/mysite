6b08230b9ec973212e510955300f5873f40429c2
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-10-08 19:30:49 +0300

    41-й коммит, новая модель и миграция БД, настройка панели администратора

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -16,3 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title']
     ordering = ['status']
     show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
